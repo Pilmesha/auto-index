c9fe07b29880f638edc7ec83ee43f58ea6166213
Pilmesha <nianozadze7@gmail.com>
2026-02-01 21:36:03 +0400

    aba

--- well2.py
+++ well2.py
@@ -157,3 +157,7 @@ def index():
 @app.route("/health")
 def health():
     return {'health':'ok'}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
